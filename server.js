77297e5844dd937988b0d54334228caf4d838e9d
khovisfersa <lucaskfsantos@gmail.com>
2022-08-31 18:38:56 -0300

    busca de grupo fix

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express()
 const porta = process.env.PORT || 3000
+var history = require('connect-history-api-fallback');
 
 app.use(express.static(__dirname + "/dist/"))
+app.use(history({
+  index: "/index.html"
+}))
 
 app.get("/", function(req, res) {
   res.sendfile(__dirname +'/dist/index.html')
